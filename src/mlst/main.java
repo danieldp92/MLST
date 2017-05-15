68cf82c6e4ba5e40d3d0262f5e0421b34fedd2b0
stefano.dallapalma <stefano.dallapalma@outlook.com>
2017-05-15 17:10:41 +0200

    modifiche

--- src/mlst/main.java
+++ src/mlst/main.java
@@ -1,16 +1,12 @@
 package mlst;
 
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
 /**
  *
  * @author Daniel
  */
 public class main {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-       
-        TestGreedy.main();
+    public static void main(String[] args) {
+      TestGreedy.main();
     }
 }
