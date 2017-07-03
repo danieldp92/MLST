fc421b6b0d625c1f73def3c34b23bcf3989a3751
orz94 <o.maitino@gmail.com>
2017-07-03 13:06:38 +0200

    Refactor

--- src/mlst/PilotNew.java
+++ src/mlst/PilotNew.java
@@ -3,16 +3,11 @@ package mlst;
 import gestore.GeneratoreGrafo;
 import gestore.XlsGrafo;
 import grafo.GrafoColorato;
-import greedy.Pilot;
+import pilot.Pilot;
 import ilog.concert.IloException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 /**
  *
@@ -34,33 +29,9 @@ public class PilotNew {
 
             GrafoColorato g = GeneratoreGrafo.generaGrafo(new File("src/GrafiColorati3Colori/" + listaGrafi.get(i)));
             int numCol = g.getColori().size();
-
-            HashMap<Integer, Integer> fr = CalcolaFrequenze(g);
-            //HashMap<Integer, Integer> pesoArchi = CalcolaPesoArchi(g, fr);
-            int sommaFr = 0;
-            for (int it = 0; it < fr.size(); it++) {
-                sommaFr += fr.get(it);
-            }
-            HashMap<Integer, Double> normfr = new HashMap<>();
-            for (int it = 0; it < fr.size(); it++) {
-                normfr.put(it, (double) fr.get(it) / sommaFr);
-            }
-            HashMap<Integer, Double> sortedNormFr = sortHashMapByValues(normfr);
-
             ArrayList<Integer> colorsToDelete = new ArrayList<>();
             int sol;
             ArrayList<ArrayList<Integer>> solutionArray = new ArrayList<>();
-
-            List<Integer> keys = new ArrayList<>(sortedNormFr.keySet());
-            /*Pilot p = new Pilot(g);
-            colorsToDelete.add(keys.get(keys.size() - 1));
-            colorsToDelete.add(keys.get(keys.size() - 2));
-            sol = p.esegui(colorsToDelete);
-            colorsToDelete.clear();
-            System.out.println("Grafo:" + listaGrafi.get(i) + " Sol:" + sol);
-            System.out.println("");
-            xls.addInfoGrafo(listaGrafi.get(i), "spilot", 0, sol);
-            xls.salva(pathTabellaRisultati);*/
 
             long startTime = System.currentTimeMillis();
 
@@ -118,14 +89,14 @@ public class PilotNew {
             }
 
             int minimumSolution = CercaMinimo(solutionArray);
-            ArrayList<ArrayList<Integer>> minimumSolutionColor = cercaMinimi(solutionArray);
+            //ArrayList<ArrayList<Integer>> minimumSolutionColor = cercaMinimi(solutionArray);
 
             float endTime = System.currentTimeMillis() - startTime;
 
             System.out.println("Grafo:" + listaGrafi.get(i) + " Sol:" + minimumSolution + " Tempo ms:" + endTime);
             System.out.println("");
 
-            xls.addInfoGrafo(listaGrafi.get(i), "pilot", endTime, minimumSolution);
+            xls.addInfoGrafo(listaGrafi.get(i), "newpilot", endTime, minimumSolution);
             xls.salva(pathTabellaRisultati);
         }
 
@@ -135,9 +106,9 @@ public class PilotNew {
     private static ArrayList<String> listaFile() {
         ArrayList<String> listaFile = new ArrayList<>();
 
-        listaFile.add("50_200_50_13_1.mlst");
+     //   listaFile.add("50_200_50_13_1.mlst");
         //Archi da 50 200 50
-    /*    for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             listaFile.add("50_200_50_13_" + i + ".mlst");
         }
 
@@ -204,7 +175,7 @@ public class PilotNew {
         //Archi da 10000 160000 10000
         for (int i = 1; i <= 5; i++) {
             listaFile.add("10000_160000_10000_625_" + i + ".mlst");
-        }*/
+        }
         return listaFile;
     }
 
@@ -239,63 +210,6 @@ public class PilotNew {
             }
         }
         return min;
-    }
-
-    private static HashMap<Integer, Integer> CalcolaFrequenze(GrafoColorato grafo) {
-        HashMap<Integer, Integer> frequenze = new HashMap<>();
-
-        for (int i = 0; i < grafo.getListaColori().size(); i++) {
-            frequenze.put(i, 0);
-        }
-
-        for (int i = 0; i < grafo.getArchi().size(); i++) {
-            frequenze.put(grafo.getArchi().get(i).getDa().getChiave(), (frequenze.get(grafo.getArchi().get(i).getDa().getChiave())) + 1);
-            frequenze.put(grafo.getArchi().get(i).getA().getChiave(), (frequenze.get(grafo.getArchi().get(i).getA().getChiave())) + 1);
-        }
-
-        return frequenze;
-    }
-
-    private static HashMap<Integer, Integer> CalcolaPesoArchi(GrafoColorato g, HashMap<Integer, Integer> frequenze) {
-        HashMap<Integer, Integer> pesoArchi = new HashMap<>();
-
-        for (int i = 0; i < g.getArchi().size(); i++) {
-            int somma = 0;
-            for (int j = 0; j < g.getArchi().get(i).getColori().size(); j++) {
-                somma += frequenze.get(g.getArchi().get(i).getColori().get(j));
-            }
-            pesoArchi.put(i, somma);
-        }
-
-        return pesoArchi;
-    }
-
-    private static LinkedHashMap<Integer, Double> sortHashMapByValues(HashMap<Integer, Double> passedMap) {
-        List<Integer> mapKeys = new ArrayList<>(passedMap.keySet());
-        List<Double> mapValues = new ArrayList<>(passedMap.values());
-        Collections.sort(mapValues);
-        Collections.sort(mapKeys);
-
-        LinkedHashMap<Integer, Double> sortedMap = new LinkedHashMap<>();
-
-        Iterator<Double> valueIt = mapValues.iterator();
-        while (valueIt.hasNext()) {
-            Double val = valueIt.next();
-            Iterator<Integer> keyIt = mapKeys.iterator();
-
-            while (keyIt.hasNext()) {
-                int key = keyIt.next();
-                double comp1 = passedMap.get(key);
-                double comp2 = val;
-
-                if (comp1 == comp2) {
-                    keyIt.remove();
-                    sortedMap.put(key, val);
-                    break;
-                }
-            }
-        }
-        return sortedMap;
     }
 
 }
