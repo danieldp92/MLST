9e665125592432ead637f7b95b4465516337e036
orz94 <o.maitino@gmail.com>
2017-07-06 15:16:16 +0200

    New Pilot with multithreading

--- src/mlst/TestPilot.java
+++ src/mlst/TestPilot.java
@@ -1,99 +1,128 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mlst;
 
 import gestore.GeneratoreGrafo;
 import gestore.XlsGrafo;
 import grafo.GrafoColorato;
 import greedy.Greedy;
+import greedy.MultiThreadGreedy;
 import ilog.concert.IloException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Dasteor
+ * @author Orazio
  */
 public class TestPilot {
-    public static void test() throws IOException, IloException {
+
+    private static final int LIVELLI = 2;
+
+    public static void test() throws IOException, IloException, InterruptedException {
 
         XlsGrafo xls = new XlsGrafo();
         String pathTabellaRisultati = "src/Risultati/TabellaRisultati.xls";
         xls.carica(pathTabellaRisultati);
-
         ArrayList<String> listaGrafi = listaFile();
 
-        //Per ogni grafo
+        //Per ogni grafico
         for (String s : listaGrafi) {
-            //Carico il grafo
-            System.out.println(s);
+
             GrafoColorato grafo = GeneratoreGrafo.generaGrafo(new File("src/GrafiColorati3Colori/" + s));
             grafo.nomeGrafo = s;
-            
             int numCol = grafo.getColori().size();
 
-            ArrayList<Integer> colorsToDelete = new ArrayList<>();
-            GrafoColorato mlst;
-            int sol;
-            ArrayList<Integer> solArray = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> solutionArray = new ArrayList<>();
+            Greedy greedy = new Greedy(grafo);
+
             long startTime = System.currentTimeMillis();
 
-            Greedy greedy = new Greedy(grafo);
-            
+            List<List<Integer>> coloriDiPartenza = new ArrayList<>();
+            ArrayList<Integer> zeta = new ArrayList<>();
+
             //Primo livello
             for (int z = 0; z < numCol; z++) {
+                List<Integer> colorsToDelete = new ArrayList<>();
                 colorsToDelete.add(z);
-                
-                mlst = greedy.esegui(colorsToDelete);
-                sol = mlst.getListaColori().size();
-                
-                solArray.add(sol);
-                colorsToDelete.clear();
+                coloriDiPartenza.add(colorsToDelete);
+                zeta.add(z);
             }
+
+            MultiThreadGreedy multi = new MultiThreadGreedy(coloriDiPartenza.size(), 50, greedy);
+            ArrayList<GrafoColorato> soluzioni = multi.avviaMultiThreadGreedy(coloriDiPartenza);
+
+            for (int i = 0; i < soluzioni.size(); i++) {
+                ArrayList<Integer> current = new ArrayList<>();
+                current.add(zeta.get(i));
+                current.add(soluzioni.get(i).getListaColori().size());
+                solutionArray.add(current);
+            }
+
+            ArrayList<ArrayList<Integer>> minimi = cercaMinimi(solutionArray);
             
-            ArrayList<Integer> mins = CercaMins(solArray); //cerca i colori con la soluzione migliore
-            solArray.clear();
+            solutionArray.clear();
+            coloriDiPartenza.clear();
 
-            //Secondo livello
-            for (int j = 0; j < mins.size(); j++) {
-                for (int z = 0; z < numCol; z++) {
-                    if (!CheckPrecedenti(z, j, mins)) {
-                        if (mins.get(j) != z) {
-                            colorsToDelete.add(mins.get(j));
-                            colorsToDelete.add(z);
-
-                            mlst = greedy.esegui(colorsToDelete);
-                            sol = mlst.getListaColori().size();
-                            
-                            solArray.add(sol);
-
-                            colorsToDelete.clear();
+            ArrayList<Integer> coloriDaAggiungere = new ArrayList<>();
+            
+            //Livelli successivi
+            for (int liv = 0; liv < LIVELLI - 1; liv++) {
+                for (int j = 0; j < minimi.size(); j++) {
+                    for (int z = 0; z < numCol; z++) {
+                        if (!CheckPrecedenti(z, j, minimi)) {
+                            if (minimi.get(j).get(0) != z) {
+                                List<Integer> colorsToDelete = new ArrayList<>();
+                                for (int index = 0; index < liv + 1; index++) {
+                                    colorsToDelete.add(minimi.get(j).get(index));
+                                    coloriDaAggiungere.add(minimi.get(j).get(index));
+                                }
+                                colorsToDelete.add(z);
+                                coloriDiPartenza.add(colorsToDelete);
+                            }
                         }
                     }
                 }
+
+                MultiThreadGreedy multi2 = new MultiThreadGreedy(coloriDiPartenza.size(), 50, greedy);
+                ArrayList<GrafoColorato> soluzioni2 = multi2.avviaMultiThreadGreedy(coloriDiPartenza);
+
+                for (int i = 0; i < soluzioni2.size(); i++) {
+                    ArrayList<Integer> current = new ArrayList<>();
+                    
+                    current.add(coloriDaAggiungere.get(i));
+                    current.add(soluzioni2.get(i).getListaColori().size());
+                    
+                    solutionArray.add(current);
+                }
+
+                minimi = cercaMinimi(solutionArray);
+                if (liv != LIVELLI - 2) {
+                    solutionArray.clear();
+                }
+
             }
 
-            int minSol = CercaMin(solArray);
-            
-            System.out.println("Numero colori: " + minSol);
-            System.out.println("Tempo di esecuzione: " + ((double)(System.currentTimeMillis() - startTime) / 1000));
+            int minimumSolution = CercaMinimo(solutionArray);
+           // ArrayList<ArrayList<Integer>> minimumSolutionColor = cercaMinimi(solutionArray);
 
-            xls.addInfoGrafo(grafo.nomeGrafo, "pilot", ((double)(System.currentTimeMillis() - startTime) / 1000), minSol);
+            float endTime = System.currentTimeMillis() - startTime;
+            float timeInSec = endTime / 1000;
+
+            System.out.println("Grafo:" + s + " Sol:" + minimumSolution + " Tempo ms:" + endTime);
+            System.out.println("");
+
+            xls.addInfoGrafo(s, "pilot", timeInSec, minimumSolution);
             xls.salva(pathTabellaRisultati);
         }
 
         xls.salva(pathTabellaRisultati);
-
     }
 
     private static ArrayList<String> listaFile() {
         ArrayList<String> listaFile = new ArrayList<>();
 
-      //  listaFile.add("50_200_50_13_1.mlst");
+        //   listaFile.add("50_200_50_13_1.mlst");
         //Archi da 50 200 50
         for (int i = 1; i <= 10; i++) {
             listaFile.add("50_200_50_13_" + i + ".mlst");
@@ -162,39 +191,41 @@ public class TestPilot {
         //Archi da 10000 160000 10000
         for (int i = 1; i <= 5; i++) {
             listaFile.add("10000_160000_10000_625_" + i + ".mlst");
-        } 
-        
+        }
         return listaFile;
     }
 
-    private static ArrayList<Integer> CercaMins(ArrayList<Integer> solArray) {
-        int min = CercaMin(solArray);
-        ArrayList<Integer> mins = new ArrayList<>();
-        for (int i = 0; i < solArray.size(); i++) {
-            if (solArray.get(i) == min) {
-                mins.add(i);
-            }
-        }
-        return mins;
-    }
-
-    private static int CercaMin(ArrayList<Integer> solArray) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < solArray.size(); i++) {
-            if (solArray.get(i) < min) {
-                min = solArray.get(i);
-            }
-        }
-        return min;
-    }
-
-    private static boolean CheckPrecedenti(int current, int index, ArrayList<Integer> precedenti) {
+    private static boolean CheckPrecedenti(int currentColor, int index, ArrayList<ArrayList<Integer>> precedenti) {
         boolean ret = false;
         for (int i = 0; i < index; i++) {
-            if (precedenti.get(i) == current) {
+            if (precedenti.get(i).get(0) == currentColor) {
                 ret = true;
             }
         }
         return ret;
     }
+
+    private static ArrayList<ArrayList<Integer>> cercaMinimi(ArrayList<ArrayList<Integer>> solutionArray) {
+        ArrayList<ArrayList<Integer>> minimi = new ArrayList<>();
+        int min = CercaMinimo(solutionArray);
+
+        for (int i = 0; i < solutionArray.size(); i++) {
+            if (solutionArray.get(i).get(solutionArray.get(i).size() - 1) == min) {
+                minimi.add(solutionArray.get(i));
+            }
+        }
+
+        return minimi;
+    }
+
+    private static int CercaMinimo(ArrayList<ArrayList<Integer>> solutionArray) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < solutionArray.size(); i++) {
+            if (solutionArray.get(i).get(solutionArray.get(i).size() - 1) < min) {
+                min = solutionArray.get(i).get(solutionArray.get(i).size() - 1);
+            }
+        }
+        return min;
+    }
+
 }
